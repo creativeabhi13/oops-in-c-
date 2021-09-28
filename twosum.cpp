6946f0a7ae035845ad7bb517978ff40c7d4c1152
ABHISHEK KUMAR <76790667+creativeabhi13@users.noreply.github.com>
2021-09-28 12:00:27 +0530

    basic

---
+++ twosum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+
+        nums[0] = 2;
+        nums[1] = 7;
+        nums[2] = 11;
+        nums[3] = 15;
+        target = nums[0] + nums[1];
+        return [ 0, 1 ];
+    }
+};
